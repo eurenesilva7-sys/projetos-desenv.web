3f728ca65ee5f6c6665fee58dccefe4f9a01f935
eurenesilva7-sys <eurenesilva7@gmail.com>
2026-09-17 20:04:33 -0300

    site pronto

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+const botao = document.getElementById('btn-tema');
 
+botao.addEventListener('click', () => {
+    alert('O botão foi clicado!');
+    document.body.classList.toggle('modo-claro');
+});
